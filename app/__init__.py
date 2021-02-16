b85498988a0a10abcb0d6a29bc0e33d90cfd2d66
Richard Schaubeck <richard.schaubeck@gmail.com>
2021-02-16 17:26:31 -0500

    configured flask with flaskenv and fleshed out file structure a little more in frontend

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,10 @@
-from app import routes
 from flask import Flask
-import os
+from app.config import Configuration
+
+app = Flask(__name__)
+app.config.from_object(Configuration)
+
+
+@app.route('/')
+def index():
+    return '<h1>Foobar</h1>'
